fcde2e3f6523c6abfc990dfb338aa72e8a14b877
Marcelo Santos <rocha.21@gmail.com>
2019-12-03 19:00:46 -0300

    configurado  nodemon e sucrase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
-  },
+    "no-console": "off",
+    semi: ["error", "never"]
+  }
 };
