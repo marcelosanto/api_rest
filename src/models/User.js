9e67f97746a91e4c3bb3be894c1090e5b699ed2d
Marcelo Santos <rocha.21@gmail.com>
2019-12-05 16:24:29 -0300

    userControle terminado

--- src/models/User.js
+++ src/models/User.js
@@ -48,7 +48,9 @@ export default class User extends Model {
     )
 
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 8)
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8)
+      }
     })
 
     return this
