1e0fb335b42dcb1ce1604e45dfb4e30bbb6051bb
Marcelo Santos <rocha.21@gmail.com>
2019-12-07 20:20:46 -0300

    criado o aluno controller

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,94 @@ class AlunoController {
     const alunos = await Aluno.findAll()
     res.json(alunos)
   }
+
+  async store(req, res) {
+    try {
+      const aluno = Aluno.create(req.body)
+      return res.json(aluno)
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      })
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        })
+      }
+
+      return res.json(aluno)
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        })
+      }
+
+      await aluno.destroy()
+      return res.json({
+        apagado: true,
+      })
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      })
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        })
+      }
+
+      const alunoAtualizado = aluno.update(req.body)
+
+      return res.json(alunoAtualizado)
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      })
+    }
+  }
 }
 
 export default new AlunoController()
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -4,5 +4,9 @@ import alunoController from '../controllers/AlunoController'
 const router = new Router()
 
 router.get('/', alunoController.index)
+router.post('/', alunoController.store)
+router.put('/:id', alunoController.update)
+router.get('/:id', alunoController.show)
+router.delete('/:id', alunoController.delete)
 
 export default router
