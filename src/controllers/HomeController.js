dbf51818dc5440981561c47c1e816640d30d2155
Marcelo Santos <rocha.21@gmail.com>
2019-12-07 20:02:16 -0300

    alteracao no cadastro de email dos alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,18 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Gabriel',
-      sobrenome: 'Santos',
-      email: 'rocha@gamicl.com',
-      idade: 9,
-      peso: 36,
-      altura: 1.40,
-    })
-    res.json({
-      novoAluno,
-    })
+    res.json('Index')
   }
 }
 
